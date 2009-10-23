a85603febfb8f53c27aa0da03de8b8905e8dccdd
Edy <edy@edy-b.de>
2009-10-23 21:17:48 +0200

    added submodules

--- s7ncms/modules/sprig-mptt/classes/sprig/field/mptt/left.php
+++
@@ -1,4 +0,0 @@
-<?php defined('SYSPATH') or die('No direct script access.');
-
-class Sprig_Field_MPTT_Left extends Sprig_Field_MPTT {
-} // End Sprig_Field_MPTT_Left
--- s7ncms/modules/sprig-mptt/classes/sprig/field/mptt/scope.php
+++
@@ -1,4 +0,0 @@
-<?php defined('SYSPATH') or die('No direct script access.');
-
-class Sprig_Field_MPTT_Scope extends Sprig_Field_MPTT {
-} // End Sprig_Field_MPTT_Scope
