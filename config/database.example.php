9928f2b208c91d259fa8a75ae7d4f7840542c42c
Edy <edy@edy-b.de>
2009-10-23 20:26:01 +0200

    added an example database config file

---
+++ config/database.example.php
@@ -0,0 +1,38 @@
+<?php defined('SYSPATH') or die('No direct script access.');
+/**
+ * S7Ncms - Open Source Content Management
+ * Copyright (c) 2007-2009, Eduard Baun <eduard at baun.de>
+ * All rights reserved.
+ *
+ * See license.txt for full text and disclaimer
+ */
+
+return array
+(
+	'default' => array
+	(
+		'type'       => 'mysql',
+		'connection' => array(
+			/**
+			 * The following options are available for MySQL:
+			 *
+			 * string   hostname
+			 * integer  port
+			 * string   socket
+			 * string   username
+			 * string   password
+			 * boolean  persistent
+			 * string   database
+			 */
+			'hostname'   => 'localhost',
+			'username'   => FALSE,
+			'password'   => FALSE,
+			'persistent' => FALSE,
+			'database'   => 's7ncms',
+		),
+		'table_prefix' => '',
+		'charset'      => 'utf8',
+		'caching'      => FALSE,
+		'profiling'    => TRUE,
+	)
+);
