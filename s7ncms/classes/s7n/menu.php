b469a0b05c5b328b54ce92bc08a6f7f344863238
Edy <eduard@baun.de>
2009-12-06 18:40:40 +0100

    deleted menu model

--- s7ncms/classes/s7n/menu.php
+++ s7ncms/classes/s7n/menu.php
@@ -18,17 +18,39 @@ class S7N_Menu {
 			$template = 'menu/all';
 		}
 
-		return new View($template, array('menu' => Sprig::factory('menu')->load_all()));
+		return new View($template, array('menu' => Sprig::factory('route')->load(NULL, FALSE)));
 	}
 
 	public static function submenu()
 	{
-		return new View('menu/all', array('menu' => Sprig::factory('menu')->submenu()));
+		$menu = NULL;
+
+		$route = Sprig::factory('route')->permalink(Request::instance()->uri());
+
+		$parents = $route->parents();
+		$count = count($parents);
+
+		if ($route->lvl === 1)
+		{
+			$menu = $route->descendants();
+		}
+		elseif ($count > 0)
+		{
+			foreach ($parents as $parent)
+			{
+				if ($parent->lvl == 1)
+				{
+					$menu = $parent->descendants();
+				}
+			}
+		}
+
+		return new View('menu/all', array('menu' => $menu));
 	}
 
 	public static function first_level()
 	{
-		return new View('menu/first_level', array('menu' => Sprig::factory('menu')->first_level()));
+		return new View('menu/first_level', array('menu' => Sprig::factory('route')->load(DB::select()->where('lvl', '=', 1), FALSE)));
 	}
 
 }
