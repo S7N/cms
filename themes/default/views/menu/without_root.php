1a35b452b5b8aeb29c46e0816cceb850306765f9
Edy <eduard@baun.de>
2009-12-07 02:08:56 +0100

    fixed a menu view

--- themes/default/views/menu/without_root.php
+++ themes/default/views/menu/without_root.php
@@ -6,45 +6,50 @@
 if ($menu !== NULL AND count($menu) > 0)
 {
 	$menu->next();
-	$level = $init_lvl = $menu->current()->lvl;
 
-	foreach ($menu as $item)
+	if ($menu->current() !== FALSE)
 	{
-		if ($item->lvl === 0)
-		{
-			continue;
-		}
+		$level = $init_lvl = $menu->current()->lvl;
 
-		$active = (urldecode($item->uri()) === Request::instance()->uri()) ? 'active' : '';
-
-		if ($item->has_children())
+		foreach ($menu as $item)
 		{
-			if ($level > $item->lvl)
+			if ($item->lvl === 0)
+			{
+				continue;
+			}
+
+			$active = (urldecode($item->uri()) === Request::instance()->uri()) ? 'active' : '';
+
+			if ($item->has_children())
+			{
+				if ($level > $item->lvl)
+				{
+					echo str_repeat("</ul></li>\n",($level - $item->lvl));
+					echo '<li class="'.$active.'">'.Html::anchor($item->uri(), $item->content->menu_title, array('class' => $active))."\n";
+					echo '<ul>'."\n";
+				}
+				else
+				{
+					echo '<li class="'.$active.'">'.Html::anchor($item->uri(), $item->content->menu_title, array('class' => $active))."\n";
+					echo '<ul>'."\n";
+				}
+			}
+			elseif ($level > $item->lvl)
 			{
 				echo str_repeat("</ul></li>\n",($level - $item->lvl));
-				echo '<li class="'.$active.'">'.Html::anchor($item->uri(), $item->content->menu_title, array('class' => $active))."\n";
-				echo '<ul>'."\n";
+				echo'<li class="'.$active.'">'.Html::anchor($item->uri(), $item->content->menu_title, array('class' => $active)).'</li>'."\n";
 			}
 			else
 			{
-				echo '<li class="'.$active.'">'.Html::anchor($item->uri(), $item->content->menu_title, array('class' => $active))."\n";
-				echo '<ul>'."\n";
+				echo '<li class="'.$active.'">'.Html::anchor($item->uri(), $item->content->menu_title, array('class' => $active)).'</li>'."\n";
 			}
-		}
-		elseif ($level > $item->lvl)
-		{
-			echo str_repeat("</ul></li>\n",($level - $item->lvl));
-			echo'<li class="'.$active.'">'.Html::anchor($item->uri(), $item->content->menu_title, array('class' => $active)).'</li>'."\n";
-		}
-		else
-		{
-			echo '<li class="'.$active.'">'.Html::anchor($item->uri(), $item->content->menu_title, array('class' => $active)).'</li>'."\n";
+
+			$level = $item->lvl;
 		}
 
-		$level = $item->lvl;
+		echo str_repeat("</ul></li>\n", ($level-$init_lvl));
 	}
 }
 
-echo str_repeat("</ul></li>\n", ($level-$init_lvl));
 ?>
 </ul>
